2ebd6e868ef724c70f0caf7baad15b92fe085bc6
ivanh <rik.chanel@gmail.com>
2022-10-09 15:48:09 +0300

    need file

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -11,47 +11,44 @@ int modulo(int a, int b) {
     return a >= 0 ? a % b : (b - abs(a % b)) % b;
 }
 
-
-
-template <typename T>
-void file(string fileRead, string fileWrite, T code) {
-    ifstream read(fileRead);
-    string k = "", msg = "";
-    while (getline(read, k))
-        msg += k;
-    read.close();
-
-    ofstream write(fileWrite);
-    if (fileWrite == "encrypt.txt")
-        write << code.encrypt(msg);
-    else
-        write << code.decrypt(msg);
-    write.close();
-}
-
 string encryptText(string text, string key) {
+
+    int a = 0, k = 0, abcSize = 256;
+    string decAbc = "", abc = "";
+    // заполнили базовый алфавит
+    for (int i = 0; i < 256; i++)
+        abc += (char)i;
+
     int len = text.length();
     int key_len = key.length();
+
     string e_text;
     for (int i = 0; i < len; i++)
-        e_text += ((text[i] + key[i % key_len]) % 128);
+        e_text += ((text[i] + key[i % key_len])% abc.length());
     return e_text;
 }
 
 string decryptText(string text, string key) {
+    int a = 0, k = 0, abcSize = 256;
+    string decAbc = "", abc = "";
+    // заполнили базовый алфавит
+    for (int i = 0; i < 256; i++)
+        abc += (char)i;
+
+
     int len = text.length();
     int key_len = key.length();
     string d_text;
     for (int i = 0; i < len; i++)
-        d_text += modulo(text[i] - key[i % key_len], 128);
+        d_text += modulo(text[i] - key[i % key_len], abc.length());
     return d_text;
 }
 
 int main(int argc, char** argv) {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
-    string text = "helloiamruslan";
-    string key = "water";
+    string text = " ЎўЈ¤Ґ¦§Ё©Є«¬­®Ї°±Ііґµ¶";
+    string key = "арбуз";
 
     string encrypted = encryptText(text, key);
     string decrypted = decryptText(encrypted, key);
